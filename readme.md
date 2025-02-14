72bfbd410656cd71cc18e0dfe4a19421c8eebb84
jgaffuri <jgaf@proton.me>
2025-02-14 09:42:30 +0100

    a

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # Rust sandbox
 
 Some rust tests. Or rest tusts.
+
+
+cargo new hello_world
+cargo build
+cargo run
+
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+//https://stevedonovan.github.io/rust-gentle-intro/1-basics.html
+//https://doc.rust-lang.org/stable/book/
+
+
+fn main() {
+
+/*
+
+    println!("Hello, World!");
+    for i in 0..5 {
+        let even_odd = if i % 2 == 0 {"even"} else {"odd"};
+        println!("{} {}", even_odd, i);
+        
+    }
+    for e in 2..3 {
+        let mut ddd = "453FDS"
+        println(ddd)
+    }
+
+*/
+
+
+
+    for i in 0..3 {
+        println!("{}", i)
+        //std::borrow
+    }
+    
+    println!("Hello, world!");
+    println!("{}", FFF)
+}
+
+static FFF:i32 = 3243;
+
