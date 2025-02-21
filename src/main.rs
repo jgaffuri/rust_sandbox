c1639ca599c5485d90481c819eef52177becde8f
jgaffuri <jgaf@proton.me>
2025-02-21 18:52:25 +0100

    a

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,32 @@ fn main() {
 
 
 
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+/*
+let user2 = User {
+    email: String::from("another@example.com"),
+    ..user1
+};*/
+
+
+
+
 /*
 
 fn main() {
