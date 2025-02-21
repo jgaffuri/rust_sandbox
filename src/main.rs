95506ca14fa910fc16de011e416ed3d098a82a51
jgaffuri <jgaf@proton.me>
2025-02-21 12:05:57 +0100

    a

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,24 @@ fn main() {
     }
 }
 
+
+
+/*
+
+fn main() {
+    let mut s = String::from("hello");
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+*/
+
+
+
+
 /*
 fn plus_one(x: i32) -> i32 {
     x + 1
