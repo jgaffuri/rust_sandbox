6784cadf004a809bc0e81400ef6f05d577b84a9d
jgaffuri <jgaf@proton.me>
2026-08-03 18:40:33 +0200

    a

--- src/ragen/graph.rs
+++ src/ragen/graph.rs
@@ -1,20 +1,20 @@
 
-pub struct Graph {
-    pub nodes: Vec<Node>,
-    pub edges: Vec<Edge>,
-    pub domains: Vec<Domain>,
+pub struct Graph<'a> {
+    pub nodes: Vec<Node<'a>>,
+    pub edges: Vec<Edge<'a>>,
+    pub domains: Vec<Domain<'a>>,
 }
 
-pub struct Node {
-    pub edges_in: Vec<&Edge>,
-    pub edges_out: Vec<&Edge>,
+pub struct Node<'a> {
+    pub edges_in: Vec<&'a Edge<'a>>,
+    pub edges_out: Vec<&'a Edge<'a>>,
 }
 
-pub struct Edge {
-    pub from: &Node,
-    pub to: &Node,
+pub struct Edge<'a> {
+    pub from: &'a Node<'a>,
+    pub to: &'a Node<'a>,
 }
 
-pub struct Domain {
-    pub edges: Vec<&Edge>,
+pub struct Domain<'a> {
+    pub edges: Vec<&'a Edge<'a>>,
 }
