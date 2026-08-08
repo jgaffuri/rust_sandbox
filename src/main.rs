52c5ff544633d2592a753f0d6c2a7fcff278f2a6
jgaffuri <jgaf@proton.me>
2026-08-08 20:08:42 +0200

    a

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use gdal::vector::{FieldValue};
 use geos::Geom;
 use std::time::Instant;
 
-use geo::algorithm::Distance;
+use geo::{Haversine, Euclidean, Distance, Point};
 use geo::{MinimumRotatedRect};
 
 pub mod ragen;
@@ -106,15 +106,17 @@ let second_len = segments.next().map(|l| l.euclidean_length());
 println!("{:?}, {:?}", first_len, second_len);
 */
 
+
 fn elongation_measure(geometry: &geo_types::Geometry<f64>) -> Result<f64> {
     let min_rot_rect = geometry.minimum_rotated_rect();
     if let Some(min_rot_rect) = min_rot_rect {
         let coords = min_rot_rect.exterior().coords().collect::<Vec<_>>();
         if coords.len() >= 4 {
-            //let width = coords[0].distance(&coords[1]);
-            //let height = coords[1].distance(&coords[2]);
-            //let width = geo_types::Point::from(coords[0]).euclidean_distance(&geo_types::Point::from(coords[1]));
-            //let height = geo_types::Point::from(coords[1]).euclidean_distance(&geo_types::Point::from(coords[2]));
+            let p0: Point = Point::new(coords[0].x, coords[0].y);
+            let p1: Point = Point::new(coords[1].x, coords[1].y);
+            let p2: Point = Point::new(coords[2].x, coords[2].y);
+            let width = Euclidean.distance(p0, p1);
+            let height = Euclidean.distance(p1, p2);
             if width > 0.0 && height > 0.0 {
                 return Ok(width.min(height) / width.max(height));
             }
