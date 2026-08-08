25eda82b19c1e0caa9885f4530a9ed4ed4b2621c
jgaffuri <jgaf@proton.me>
2026-08-08 14:46:50 +0200

    a

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,9 @@ fn main() -> Result<()> {
         //print_type_of(&f.geometry);
         //f.geometry = geo_types::Geometry::MultiPolygon(f.geometry.buffer(50.0));
 
-        let g = f.geometry.buffer(5000.0, 2)?;
-        let g = ragen::geom_utils::to_multi(g)?;
-        f.geometry = g;
+        //let g = f.geometry.buffer(5000.0, 2)?;
+        //let g = ragen::geom_utils::to_multi(g)?;
+        //f.geometry = g;
 
         let circularity = 4.0 * std::f64::consts::PI * f.geometry.area()? / (f.geometry.length()? * f.geometry.length()?);
         f.attributes.insert("circularity".to_string(), FieldValue::IntegerValue((circularity*100.0).round() as i32));
