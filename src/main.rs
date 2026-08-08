600692438bbdc3d27523605d6d90e0704bb269d7
jgaffuri <jgaf@proton.me>
2026-08-08 21:34:53 +0200

    a

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use gdal::vector::{FieldValue};
 use geos::Geom;
 use std::time::Instant;
 
-use geo::{Euclidean, Distance, Point};
 use geo::{MinimumRotatedRect};
 
 pub mod ragen;
@@ -14,7 +13,6 @@ use crate::ragen::geom_utils::{geo_to_geos, geos_to_geo};
 
 /*
 TODO
-elongation measure
 improve geo-geos conversion: choice. test performance ?
 graph model
 translate some geometrical algorithms ?
@@ -97,32 +95,3 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-/*
-use geo::EuclideanLength;
-
-let mut segments = linestring.lines();
-
-let first_len = segments.next().map(|l| l.euclidean_length());
-let second_len = segments.next().map(|l| l.euclidean_length());
-
-println!("{:?}, {:?}", first_len, second_len);
-*/
-
-
-fn elongation_measure(geometry: &geo_types::Geometry<f64>) -> Result<f64> {
-    let min_rot_rect = geometry.minimum_rotated_rect();
-    if let Some(min_rot_rect) = min_rot_rect {
-        let coords = min_rot_rect.exterior().coords().collect::<Vec<_>>();
-        if coords.len() >= 4 {
-            let p0: Point = Point::new(coords[0].x, coords[0].y);
-            let p1: Point = Point::new(coords[1].x, coords[1].y);
-            let p2: Point = Point::new(coords[2].x, coords[2].y);
-            let width = Euclidean.distance(p0, p1);
-            let height = Euclidean.distance(p1, p2);
-            if width > 0.0 && height > 0.0 {
-                return Ok(width.min(height) / width.max(height));
-            }
-        }
-    }
-    Ok(0.0)
-}
--- src/ragen/geom_utils.rs
+++ src/ragen/geom_utils.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use geos::{Geom, Geometry, GeometryTypes};
 use wkt::{ToWkt, TryFromWkt};
+use geo::{Euclidean, Distance, Point, MinimumRotatedRect};
 
 
 
@@ -31,4 +32,24 @@ pub fn to_multi(geom: Geometry) -> Result<Geometry, geos::Error> {
         | GeometryTypes::MultiPolygon
         | GeometryTypes::GeometryCollection => Ok(geom),
     }
+}
+
+
+
+pub fn elongation_measure(geometry: &geo_types::Geometry<f64>) -> Result<f64> {
+    let min_rot_rect = geometry.minimum_rotated_rect();
+    if let Some(min_rot_rect) = min_rot_rect {
+        let coords = min_rot_rect.exterior().coords().collect::<Vec<_>>();
+        if coords.len() >= 4 {
+            let p0: Point = Point::new(coords[0].x, coords[0].y);
+            let p1: Point = Point::new(coords[1].x, coords[1].y);
+            let p2: Point = Point::new(coords[2].x, coords[2].y);
+            let width = Euclidean.distance(p0, p1);
+            let height = Euclidean.distance(p1, p2);
+            if width > 0.0 && height > 0.0 {
+                return Ok(width.min(height) / width.max(height));
+            }
+        }
+    }
+    Ok(0.0)
 }
