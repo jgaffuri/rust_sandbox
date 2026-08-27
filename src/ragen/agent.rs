9c747a91d8bf8f4a3521edb6f406d42de3f800e1
jgaffuri <jgaf@proton.me>
2026-08-27 19:23:01 +0200

    a

--- src/ragen/agent.rs
+++ src/ragen/agent.rs
@@ -13,3 +13,25 @@ pub trait compute_statisfaction {
     fn compute_statisfaction(&mut self);
 }
  */
+
+impl Agent {
+    pub fn new() -> Self {
+        Agent {
+            statisfaction: 0,
+            constraints: [Constraint { statisfaction: 0 }],
+        }
+    }
+
+    pub fn compute_statisfaction(&mut self) {
+        if self.constraints.is_empty() {
+            self.statisfaction = 10;
+            return;
+        }
+        let mut total_statisfaction: i16 = 0;
+        for constraint in &self.constraints {
+            total_statisfaction += constraint.statisfaction as i16;
+        }
+        total_statisfaction = total_statisfaction / self.constraints.len() as i16;
+        self.statisfaction = total_statisfaction as i8;
+    }
+}
