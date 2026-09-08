cdfee6a3ec0fd7b9b26d3b6c3ee6ff4efba1dc24
jgaffuri <jgaf@proton.me>
2026-09-08 20:34:24 +0200

    a

--- src/gene/base.rs
+++ src/gene/base.rs
@@ -1,8 +1,40 @@
+use crate::ragen::agent::{Constraint, ConstraintStruct};
+
 
 
 // A constraint for surfacic agents to have a minimum size.
 pub struct SizeConstraint {
+    pub constraint_data: ConstraintStruct
+}
 
+
+impl Constraint for SizeConstraint {
+    fn get_agent(&self) -> &crate::ragen::agent::Agent { &self.constraint_data.agent }
+    fn get_importance(&self) -> f64 { self.constraint_data.importance }
+    fn get_priority(&self) -> i8 { self.constraint_data.priority }
+    fn get_satisfaction(&self) -> f64 { self.constraint_data.statisfaction }
+    fn set_satisfaction(&self, satisfaction: f64) { self.constraint_data.statisfaction = satisfaction; }
+    fn is_hard(&self) -> bool { false }
+
+    fn compute_initial_value(&self) {
+        todo!()
+    }
+
+    fn compute_current_value(&self) {
+        todo!()
+    }
+
+    fn compute_goal_value(&self) {
+        todo!()
+    }
+
+    fn compute_satisfaction(&self) {
+        todo!()
+    }
+
+    fn get_transformations(&self) -> Vec<Box<dyn crate::ragen::agent::Transformation>> {
+        todo!()
+    }
 
 }
 
